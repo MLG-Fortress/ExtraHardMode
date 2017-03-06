8966bb0c657f253e318af696bb19bc48cd33f34e
Erik1988 <erik.haugsnes@gmail.com>
2017-03-06 07:39:22 +0100

    Added the option to allow certian mobs that dont normally spawn.
    vex (caves), guardians(ocian and deep ocian), Vindicator (roofed forest)
    and Cave spider (Swampland)

--- src/main/java/com/extrahardmode/ExtraHardMode.java
+++ src/main/java/com/extrahardmode/ExtraHardMode.java
@@ -149,14 +149,14 @@ public class ExtraHardMode extends JavaPlugin
         registerModule(Witches.class, new Witches(this));
         
         //Disables MobVariation
-        //boolean dvar = config.getBoolean("ExtraHardMode.Disable.MobVariation",false);
-		//if(!dvar){
-        //registerModule(KillerBunny.class, new KillerBunny(this));
-        //registerModule(Vindicator.class, new Vindicator(this));
-        //registerModule(CaveSpider.class, new CaveSpider(this));
-        //registerModule(Guardians.class, new Guardians(this));
-        //registerModule(Vex.class, new Vex(this));
-		//}
+        boolean dvar = config.getBoolean("ExtraHardMode.Disable.MobVariation",false);
+		if(!dvar){
+        registerModule(KillerBunny.class, new KillerBunny(this));
+        registerModule(Vindicator.class, new Vindicator(this));
+        registerModule(CaveSpider.class, new CaveSpider(this));
+        registerModule(Guardians.class, new Guardians(this));
+        registerModule(Vex.class, new Vex(this));
+		}
         //Compatibility
         registerModule(CompatHandler.class, new CompatHandler(this));
         registerModule(ExplosionCompatStorage.class, new ExplosionCompatStorage(this));
